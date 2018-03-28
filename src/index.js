6f45d890a5f70b9ada3509136a3559b30b9825d1
cullenm <cmarchenko@csumb.edu>
2018-03-28 09:18:55 +0000

    obtaining text from textbox and storing in state variable

--- src/index.js
+++ src/index.js
@@ -7,11 +7,19 @@ class UserListComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      textInputValue1: '',
+      textInputValue2: '',
       userName: null,
       userProfImg: null,
       userLoc: null,
-      
+      textVal: '',
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.testFunc = this.testFunc.bind(this);
+  }
+  
+  handleChange(event) {
+    this.setState({textVal: event.target.value});
   }
   
   componentDidMount() {
@@ -30,10 +38,26 @@ class UserListComponent extends React.Component {
       }.bind(this));
   }
   
+  testFunc() {
+    alert("::" + this.state.textVal);
+    
+    
+    return;
+  }
+  
+  
   render() {
     return (
+      
       <div id="layout-content" className="layout-content-wrapper">
-        <div className="panel-list">{ this.state.userName + " " + this.state.userLoc }</div>
+        <div className="na">{ this.state.userName + " " + this.state.userLoc }</div>
+        <form>
+          <label>
+            Username:
+            <input type="text" value={this.state.textVal} onChange={this.handleChange} />
+          </label>
+          <input type="button" value="|S|E|A|R|C|H|" onClick={this.testFunc}/>
+        </form>
       </div>
     );
   }
