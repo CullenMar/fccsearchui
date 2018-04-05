8bc599e3c392b3c71caecdc09d34c82afe764493
cullenm <cmarchenko@csumb.edu>
2018-04-05 06:31:18 +0000

    got the table to render properly, table was not rendering upon recieving a new student

--- src/index.js
+++ src/index.js
@@ -12,58 +12,111 @@ class UserListComponent extends React.Component {
       userName: null,
       userProfImg: null,
       userLoc: null,
+      studentList: [],
       textVal: '',
+      msg: '',
     };
     this.handleChange = this.handleChange.bind(this);
-    this.testFunc = this.testFunc.bind(this);
+    this.scraperSearch = this.scraperSearch.bind(this);
+    this.onKeyPress = this.onKeyPress.bind(this);
   }
   
-  handleChange(event) {
-    this.setState({textVal: event.target.value});
+  handleChange(e) {
+    this.setState({textVal: e.target.value});
   }
   
-  componentDidMount() {
-    this.UserList();
-  }
-  
-  UserList() {
-    $.getJSON('http://fcc-profile-scraper.herokuapp.com/user/user512')
+  //handles scraper requests
+  scraperSearch() {
+    var input = this.state.textVal;
+    $.getJSON('http://fcc-profile-scraper.herokuapp.com/user/' + input)
       .done(function(data){
-        console.log(data);
         this.setState({
-          userName: data.name,
+          userName: "Name: " + data.name,
           userProfImg: data.profileImage,
-          userLoc: data.location
+          userLoc: data.location,
+          msg: '',
+          studentList: [...this.state.studentList, data],
         });
-      }.bind(this));
+    }.bind(this)).fail(function() {
+      this.setState({
+        userName: '',
+        userProfImg: '',
+        userLoc: '',
+        msg: input + ' does not exist.',
+      })
+    }.bind(this));
   }
   
-  testFunc() {
-    alert("::" + this.state.textVal);
-    
-    
-    return;
+  onKeyPress(event) {
+    if (event.which === 13 /* Enter */) {
+      event.preventDefault();
+      this.scraperSearch();
+    }
   }
   
-  
+  //create the html
   render() {
     return (
-      
       <div id="layout-content" className="layout-content-wrapper">
-        <div className="na">{ this.state.userName + " " + this.state.userLoc }</div>
         <form>
           <label>
+            <div className="message">{ this.state.msg }</div>
             Username:
-            <input type="text" value={this.state.textVal} onChange={this.handleChange} />
+            <input type="text" value={this.state.textVal} onChange={this.handleChange} onKeyPress={this.onKeyPress} />
           </label>
-          <input type="button" value="|S|E|A|R|C|H|" onClick={this.testFunc}/>
+          <input type="button" value="SEARCH" onClick={this.scraperSearch}/>
+          <br />
+          <DisplayUser userData={this.state.studentList} userNameD={this.state.userName} userProfImgD={this.state.userProfImg} userLocD={this.state.userLoc} />
         </form>
       </div>
     );
   }
 }
 
-
+class DisplayUser extends React.Component {
+  constructor(props) {
+    super(props);
+    this.htmlStuff = '';
+    this.htmlTest = "<tr> <td>Smill Smates</td> <td>stuff3</td> <td>stuff4</td> </tr>";
+  }
+  
+  handleRemove(x) {
+    
+  }
+  
+  render() {
+    let rows = [];
+    let counter = 0;
+    console.log(this.props.userData.length);
+    while (this.props.userData.length > counter) {
+      this.htmlStuff = this.htmlStuff + "<tbody><tr><td>" + this.props.userData[counter].name + "</td><td>" + this.props.userData[counter].profileImage + "</td><td>" + this.props.userData[counter].location + "</td></tr></tbody>";
+      let cell = [];
+      cell.push(<td>{this.props.userData[counter].name}</td>);
+      cell.push(<td><img className ="imgs" src={this.props.userData[counter].profileImage} alt="Unavailable" /></td>);
+      cell.push(<td>{this.props.userData[counter].location}</td>);
+      cell.push(<td><input type="button" value="Remove" onClick={this.handleRemove(counter)}/></td>);
+      rows.push(<tr>{cell}</tr>);
+      counter = counter + 1;
+    }
+    return (
+      <div id="layout-content" className="layout-content-wrapper">
+        <table className="studentTable">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Picture</th>
+            <th>Location</th>
+            <th>Remove</th>
+          </tr>
+        </tbody>
+        <tbody>
+          {rows}
+        </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 // ========================================
 
